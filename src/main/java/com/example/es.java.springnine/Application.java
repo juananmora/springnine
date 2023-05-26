a66b8d1046f17c6c72044c6605ad368e0f84d4fa
Jon Mora Alonso <52193917+juananmora@users.noreply.github.com>
2023-05-26 12:46:54 +0200

    Update Application.java

--- src/main/java/com/example/es.java.springnine/Application.java
+++ src/main/java/com/example/es.java.springnine/Application.java
@@ -3,6 +3,8 @@ package com.example.es.java.springnine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+public String password="";
+
 @SpringBootApplication
 public class Application {
 
