41260d126815dd45caa37cc20ded0b8f1e188676
Jon Mora Alonso <52193917+juananmora@users.noreply.github.com>
2023-06-04 12:30:08 +0200

    user story #US001 #1001

--- src/main/java/com/example/es.java.springnine/Application.java
+++ src/main/java/com/example/es.java.springnine/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
   public String password="";
-  public String pwd="";
+  public String pwd="hola";
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
